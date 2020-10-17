ee287b33e8af1923df469e180ea6ccb604a3618d
unknown <assma.bm@gmail.com>
2020-10-17 18:21:03 +0300

    initial commit

---
+++ Assets/Hacker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPage();
+    }   
+    void startingPage()
+    {
+        string intro = "Ouuu I'm feeling quite penetrable today ;)" +
+                           "\nWhich part of me are you interested in hacking?";
+        string question = "\nPress A to hack my grocery store" +
+                           "\nPress B to hack my hospital" +
+                           "\nPress C to hack the CIA";
+        Terminal.WriteLine(intro);
+        Terminal.WriteLine(question);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
